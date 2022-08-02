63233c40ad3c16981dce19682bd58530639214f9
Timex19 <ojooluwtimilehinpeter@yahoo.com>
2022-08-02 12:37:01 -0700

    Adding task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,15 +8,15 @@ def rotate_2d_matrix(matrix):
     rotate two dimension matrix 90 degrees clockwise
     Args: matrix (list[[list]]): a matrix
     """
-    n = len(matrix
-            for i in range(int(n / 2)):
-            y = (n - i - 1)
-            for j in range(i, y):
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
             x = (n - 1 - j)
             # current number
             tmp = matrix[i][j]
             # change top for left
-            matrix[i][j] = matix[x][i]
+            matrix[i][j] = matrix[x][i]
             # change left for bottom
             matrix[x][i] = matrix[y][x]
             # change bottom for right
